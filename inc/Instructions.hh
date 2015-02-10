caf6d5cec9e4292eab1f22a06881ffbf8a39f58c
grout_r <roman.grout@epitech.eu>
2015-02-10 15:22:26 +0100

    commit

--- inc/Instructions.hh
+++ inc/Instructions.hh
@@ -2,15 +2,18 @@
 #include <vector>
 #include <stack>
 #include <string>
+#include <map>
 
 typedef struct  s_instruction
 {
     void        (*funcptr)(std::stack <int>operand);
+    std::string input;
 }               t_instruction;
 
 class           Instructions
 {
 private:
+    std::map    <std::string, void *>db;
     std::vector  <std::string>instructions;
 
 public:
